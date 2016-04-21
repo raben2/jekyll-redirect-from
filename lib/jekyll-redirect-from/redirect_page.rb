26799741dc42ded8d812e026f5737b8dca8daf55
Georg Rabenstein <georg.rabenstein@deposit-solutions.com>
2016-04-21 11:12:46 +0200

    redirect without html

--- lib/jekyll-redirect-from/redirect_page.rb
+++ lib/jekyll-redirect-from/redirect_page.rb
@@ -28,11 +28,8 @@ module JekyllRedirectFrom
       self.output = self.content = <<-EOF
 <!DOCTYPE html>
 <meta charset="utf-8">
-<title>Redirecting…</title>
 <link rel="canonical" href="#{item_url}">
 <meta http-equiv="refresh" content="0; url=#{item_url}">
-<h1>Redirecting…</h1>
-<a href="#{item_url}">Click here if you are not redirected.</a>
 <script>location="#{item_url}"</script>
 EOF
     end
